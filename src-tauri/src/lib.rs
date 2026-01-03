b97362a9dcb6098531076587903427be0219c14e
Alexandre Trotel <trotelalexandre@proton.me>
2026-01-03 18:23:48 +0100

    feat: add tauri-plugin-notification for enhanced notification support

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,6 +9,7 @@ use tauri_plugin_window_state::{AppHandleExt, StateFlags};
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(plugins::logging::build().build())
         .setup(|app| {
