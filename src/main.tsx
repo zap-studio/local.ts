0da658b4a2da5a86efe99f0599367a8697fc0ca8
Alexandre Trotel <trotelalexandre@proton.me>
2026-01-03 17:41:41 +0100

    feat: add Toaster component from sonner for improved notification handling

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Toaster } from "sonner";
 
 import { StoreInitializer } from "@/components/store-initializer";
 
@@ -19,6 +20,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <StoreInitializer>
       <RouterProvider router={router} />
+      <Toaster position={"bottom-right"} />
     </StoreInitializer>
   </React.StrictMode>
 );
