df2d6a762ab6879fe901e76bf209304b5b0c2530
Alexandre Trotel <trotelalexandre@proton.me>
2026-01-03 10:41:48 +0100

    chore: add tsconfig paths vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
